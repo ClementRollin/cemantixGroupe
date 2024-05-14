2ed6c9acee556fae0862f3e52baa48461d50de66
Alexyo42 <120744066+Alexyo42@users.noreply.github.com>
2024-05-14 11:28:53 +0200

    root plus timeOut sur le loading

---
+++ CemantixGrp/src/Loading.js
@@ -0,0 +1,9 @@
+import {Text, View} from "react-native";
+
+export default function Loading() {
+    return (
+        <View style={styles.container}>
+            <Text>Loading</Text>
+        </View>
+    );
+}
